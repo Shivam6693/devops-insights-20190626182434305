a9d904118d1f6b9219c47be87dae1a29e962ee60
Umesh Parulekar <ucparule@us.ibm.com>
2017-02-24 14:50:26 -0600

    oneibmcloud/zenhub#1470 corrected fvts

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -5,41 +5,60 @@
 
     var apiv1 = require('../../routes/apiv1');
     var assert = require('chai').assert;
-    var express = require('express');
-    var rqst = require('supertest');
+    var REQUEST = require('request');
 
-    var app = express();
+    var request = REQUEST.defaults( {
+        strictSSL: false
+    });
+
+    var appUrl = process.env.APP_URL;
 
     describe('Get Weather', function() {
 
-    	before(function() {
-            app.use('/api/v1/', apiv1.router);
-        });
-
     	it('with valid zip code', function(done) {
-    		rqst(app).get('/api/v1/getWeather?zip=78613')
-    		.end(function(err, res) {
-                    assert.equal(res.status, 200);
-                    assert(((res.body.city === 'Anderson Mill') || (res.body.city === 'Cedar Park')), "City name does not match");
-                    done();
-                });
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?zip=78613'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert(((pbody.city === 'Anderson Mill') || (pbody.city === 'Cedar Park')), "City name does not match");
+              done();
+            }
+        });
     	});
 
-    	it('with without zip code', function(done) {
-    		rqst(app).get('/api/v1/getWeather')
-    		.end(function(err, res) {
-                    assert.equal(res.status, 400);
-                    done();
-                });
+      it('without zip code', function(done) {
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 400);
+              done();
+            }
+        });
     	});
 
-    	it('with another valid zip code', function(done) {
-    		rqst(app).get('/api/v1/getWeather?zip=78641')
-    		.end(function(err, res) {
-                    assert.equal(res.status, 200);
-                    assert.equal(res.body.city, 'Leander', "City name does not match");
-                    done();
-                });
+      it('with another valid zip code', function(done) {
+        request({
+      		method: 'GET',
+              url: appUrl + '/api/v1/getWeather?zip=78641'
+          }, function(err, resp, body) {
+          	if(err) {
+          		assert.fail('Failed to get the response');
+          	} else {
+              assert.equal(resp.statusCode, 200);
+              var pbody = JSON.parse(body);
+              assert((pbody.city === 'Leander'), "City name does not match");
+              done();
+            }
+        });
     	});
     });
 }());
